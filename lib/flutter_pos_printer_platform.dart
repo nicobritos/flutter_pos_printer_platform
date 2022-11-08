4fbf7527406c360f3f462723b60a17d4ced99c99
Nicolas Britos <nicbritos@hotmail.com>
2022-11-08 12:18:51 -0300

    chore: remove star printer support

--- lib/flutter_pos_printer_platform.dart
+++ lib/flutter_pos_printer_platform.dart
@@ -1,12 +1,11 @@
 library flutter_pos_printer_platform;
 
-export './src/connectors/usb.dart';
-export './src/connectors/bluetooth.dart';
-export './src/utils.dart';
-export './src/connectors/tcp.dart';
-export './src/printers/escpos.dart';
-export './src/printers/star.dart';
-export './src/printers/tspl.dart';
 export './printer.dart';
-export './src/printer_manager.dart';
+export './src/connectors/bluetooth.dart';
+export './src/connectors/tcp.dart';
+export './src/connectors/usb.dart';
 export './src/models/printer_device.dart';
+export './src/printer_manager.dart';
+export './src/printers/escpos.dart';
+export './src/printers/tspl.dart';
+export './src/utils.dart';
